8d99f885ee00a11a45ea5a27c2926ae25ca842c6
Andry_art <asosonko93@gmail.com>
2021-12-18 14:59:08 +0300

    first changes

--- packages/App/src/components/Input.tsx
+++ packages/App/src/components/Input.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {ChangeEvent, FC} from 'react';
 import {View, StyleSheet, TextInput, Text, TextInputProps} from 'react-native';
 
@@ -12,32 +11,15 @@ const Input: FC<Props> = ({label, change, ...textInputProps}) => {
     change(e);
   };
 
-=======
-import React, {FC} from 'react';
-import {View, StyleSheet, TextInput, Text} from 'react-native';
-
-interface Props {
-  label: string;
-  secur?: boolean;
-}
-
-const Input: FC<Props> = ({label, secur}) => {
->>>>>>> aa552c8358226dc46b3958003014942e5a6ea604
   return (
     <View style={styles.inputArea}>
       <Text style={styles.labels}>{label}</Text>
       <TextInput
-<<<<<<< HEAD
         {...textInputProps}
         style={styles.inputs}
         onChangeText={e => handleChange(e)}
         // textContentType={content}
         // secureTextEntry={secur}
-=======
-        style={styles.inputs}
-        // textContentType={content}
-        secureTextEntry={secur}
->>>>>>> aa552c8358226dc46b3958003014942e5a6ea604
       />
     </View>
   );
